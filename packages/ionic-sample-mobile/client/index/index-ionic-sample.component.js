f5b56997ce28fbdbe2ae23691cdf2e2676c9882f
Pedro Luis Furio Raphael <pedroluis.raphael@gmail.com>
2016-01-17 18:29:49 -0200

    slide box no login

--- packages/ionic-sample-mobile/client/index/index-ionic-sample.component.js
+++ packages/ionic-sample-mobile/client/index/index-ionic-sample.component.js
@@ -3,8 +3,10 @@ angular.module('ionic-sample.mobile').directive('indexIonicSample', function () 
     restrict: 'E',
     templateUrl: '/packages/ionic-sample-mobile/client/index/index-ionic-sample.html',
     controllerAs: 'index',
-    controller: function ($scope, $reactive, $state) {
+    controller: function ($scope, $reactive, $state, $ionicSideMenuDelegate) {
       $reactive(this).attach($scope);
+
+      $ionicSideMenuDelegate.canDragContent(false);
 
       this.logout = () => {
         Accounts.logout();
