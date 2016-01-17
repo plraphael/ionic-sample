13bc75a82c344a23fe4523841671afe317870aa2
Pedro Luis Furio Raphael <pedroluis.raphael@gmail.com>
2016-01-17 08:54:52 -0200

    tabs and side menu working properly together now

--- packages/ionic-sample-mobile/client/lib/module.js
+++ packages/ionic-sample-mobile/client/lib/module.js
@@ -1,3 +1,6 @@
 angular.module('ionic-sample.mobile', [
   'ionic',
-]);
+]).config(($ionicConfigProvider) => {
+  $ionicConfigProvider.views.maxCache(5); 
+  $ionicConfigProvider.tabs.position('bottom')
+});
